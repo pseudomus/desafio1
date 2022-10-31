0b744bc669b2c4eab0b64c4de52364690e406f4c
pseudomus <102067779+pseudomus@users.noreply.github.com>
2022-10-31 11:21:11 -0300

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,9 +1,56 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#define TAMPILHA 10;
 
-int main(void){
+int pilha[TAMPILHA]={0,0,0,0,0,0,0,0,0,0};
+int topo=0;
+
+void lista_elementos(){
 	
-	printf("123");
-	printf("Hello word");
+	printf("Pilha Atual:\n");
+	for(int i=0;i<TAMPILHA;i++){
+		printf("-");
+		printf("|%d|", pilha[i]);
+		printf("-");
+	}
+	printf("\n");
+	printf("Topo: %d\n", topo);
+}
+
+void push(){
+	if(topo<TAMPILHA){
+		printf("Informe um valor: ");
+		scanf("%d", pilha[topo]);
+		topo++;
+	}
+	else{
+		printf("A pilha está cheia.\n");
+	}
+}
+
+void pop(){
+	if(topo>0){
+		pilha[topo-1]=0;
+		topo--;
+	}
+	else{
+		printf("A pilha está vazia.\n");
+	}
+}
+
+void clear(){
+	for(int i=0;i<TAMPILHA;i++){
+		pilha[i]=0;
+	}
+	topo=0;
+}
+
+main(){
+	setlocale(LC_ALL, "");
+	int opcao;
 	
-	return 0;
+	do{
+		printf("--------------------------------")
+	}while(opcao!=5);
 }
